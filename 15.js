fa9e4376bd8c0ea634a9b3cc471727356f5402c1
Jose Manuel Padilla Caro <a310901@uach.mx>
2019-09-19 11:55:00 -0600

    Problema 15 corregido

--- 15.js
+++ 15.js
@@ -3,7 +3,7 @@ inicio = readline.question("Introduce el numero en el que quieres inciar: ");
 final = readline.question("Introduce el numero en el que quieres terminar: ");
 let contador=0;
 let evaluarPares=0;
-let sumaPares=0;
+let sumaImpares=0;
 let contadorPares=0;
 for(i= inicio; i<=final ; i++){
     console.log(i);
@@ -12,8 +12,9 @@ for(i= inicio; i<=final ; i++){
     evaluarPares = i%2;
     if(evaluarPares ==0){
         contadorPares++;
-        sumaPares= parseInt(sumaPares) +i;
+    }else{
+        sumaImpares= parseInt(sumaImpares) +i;
     }
 }
 console.log("Esta es la cantidad de pares que hay: " + contadorPares);
-console.log("la suma de los pares es: " + sumaPares);
+console.log("la suma de los impares es: " + sumaImpares);
