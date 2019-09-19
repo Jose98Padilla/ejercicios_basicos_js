2ddc590071f022f75bda0163efc5f1def76ddf5c
Jose Manuel Padilla Caro <a310901@uach.mx>
2019-09-19 12:34:18 -0600

    Problema 19

---
+++ 19.js
@@ -0,0 +1,11 @@
+console.log("Simulacion de comportamiento de Reloj Digital:");
+
+for (h = 00; h <= 23; h++){
+    for (m = 00; m <= 59; m++){
+        for (s = 00; s <= 59; s++){
+            console.log(h + ":" + m + ":" + s);
+            //console.clear();
+        }
+    }
+}
+console.log("Simulacion terminada");
